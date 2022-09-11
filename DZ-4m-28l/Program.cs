d847dd226710d7daa231ef1b155c1e5c14a42433
admin <admin@DESKTOP-TOG76HK>
2022-09-11 23:15:12 +0800

    Add project files.

---
+++ DZ-4m-28l/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    //Директории файлов
+    static string path = "c:\\Otus\\TestDir1";
+    static string path2 = "c:\\Otus\\TestDir2";
+
+
+
+    private static void Main(string[] args)
+    {
+        Start(path);
+        Start(path2);
+    }
+
+    public static void Start(string pathIn)
+    {
+        
+        DirectoryInfo createDir = new DirectoryInfo(pathIn);
+        createDir.Create();
+        for (int i = 0; i < 10; i++)
+        {
+            string pathFile = $"{pathIn}\\File{i+1}.txt";
+            
+            using (StreamWriter sw = File.CreateText(pathFile))
+            {
+                sw.WriteLine("Hello");
+                sw.WriteLine("And");
+                sw.WriteLine("Welcome");
+            }
+            
+        }
+    }
+
+
+
+}
